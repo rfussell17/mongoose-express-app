5fcbcf520374beab1e699e26d431d48f05a8873b
Robin Fussell <rfussell17@gmail.com>
2021-05-12 16:09:25 -0700

    created product model

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true})
+  .then(()=>{
+    console.log('mongo connection open')
+  })
+  .catch(err =>{
+    console.log('mongo connection error')
+    console.log(err)
+  })
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
