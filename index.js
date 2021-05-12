7bf15d46f60538a414a53dd0d2427575514ee59b
Robin Fussell <rfussell17@gmail.com>
2021-05-12 15:05:30 -0700

    app listening, generic route working correctly on localhost 3000/dog

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/dog', (req, res) => {
+  res.send('woof')
+})
+
 app.listen(3000, () => {
   console.log('listening on port 3000')
 })
