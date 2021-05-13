134caac96f10527720f350c243c6e7ee9c25ef8f
Robin Fussell <rfussell17@gmail.com>
2021-05-13 15:56:37 -0700

    formatted server file

--- index.js
+++ index.js
@@ -1,68 +1,73 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
+const Product = require("./models/product");
 
-const Product = require('./models/product');
-
-mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true})
-  .then(()=>{
-    console.log('mongo connection open')
+mongoose
+  .connect("mongodb://localhost:27017/farmStand", { useNewUrlParser: true })
+  .then(() => {
+    console.log("mongo connection open");
   })
-  .catch(err =>{
-    console.log('mongo connection error')
-    console.log(err)
-  })
+  .catch((err) => {
+    console.log("mongo connection error");
+    console.log(err);
+  });
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-const categories = ['fruit', 'vegetables', 'dairy'];
+const categories = ["fruit", "vegetables", "dairy"];
 
-app.get('/products', async (req, res) => {
-  const products = await Product.find({})
-  console.log(products)
-  res.render('products/index', { products })
-})
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  console.log(products);
+  res.render("products/index", { products });
+});
 
-app.get('/products/new', (req, res) => {
-  res.render('products/new', { categories })
-})
+app.get("/products/new", (req, res) => {
+  res.render("products/new", { categories });
+});
 
-app.post('/products', async (req, res) => {
-  const newProduct = new Product(req.body)
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
   await newProduct.save();
-  res.redirect(`/products/${newProduct._id}`)
-})
+  res.redirect(`/products/${newProduct._id}`);
+});
 
-app.get('/products/:id', async (req, res) => {
+app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
-  const product = await Product.findById(id)
-  console.log(product)
-  res.render('products/show', { product })
-})
+  const product = await Product.findById(id);
+  console.log(product);
+  res.render("products/show", { product });
+});
 
-app.get('/products/:id/edit', async (req, res) => {
+app.get("/products/:id/edit", async (req, res) => {
   const { id } = req.params;
-  const product = await Product.findById(id)
-  res.render('products/edit', { product, categories })
-})
+  const product = await Product.findById(id);
+  res.render("products/edit", { product, categories });
+});
 
-app.put('/products/:id', async (req, res) => {
+app.put("/products/:id", async (req, res) => {
   const { id } = req.params;
-  const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
   res.redirect(`/products/${product._id}`);
-})
+});
 
-app.delete('/products/:id', async (req, res) => {
+app.delete("/products/:id", async (req, res) => {
   const { id } = req.params;
   const deletedProduct = await Product.findByIdAndDelete(id);
-  res.redirect('/products')
-})
+  res.redirect("/products");
+});
 
-
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
